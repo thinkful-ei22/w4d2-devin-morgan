7e10b74fa19d76d083af1ade2ce471dd0f5ba704
dghero <devingreyhero@gmail.com>
2018-07-03 11:41:47 -0500

    Complete knex drill exercises

--- queries.js
+++ queries.js
@@ -50,3 +50,78 @@ process.stdout.write('\x1Bc');
 //   .where('cuisine', 'Thai')
 //   .andWhere('address_zipcode', '11372')
 //   .then(result => console.log(result));
+
+//list Italian restaurants with zip code 10012, 10013, or 10014
+// knex('restaurants')
+//   .select('id', 'name')
+//   .whereIn('address_zipcode', [10012, 10013, 10014])
+//   .andWhere('cuisine','Italian')
+//   .orderBy('name')
+//   .limit(5)
+//   .then(result => console.log(result));
+
+//Create a restaurant
+// knex('restaurants')
+//   .insert({name: 'Byte Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'coffee',
+//     address_building_number: '123',
+//     address_street: 'Atlantic Avenue',
+//     address_zipcode: '11231'})
+//   .then(result => console.log(result));
+
+//Create a restaurant then return id and name
+// knex('restaurants')
+//   .insert({name: 'Sunlit Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'breakfast',
+//     address_building_number: '444',
+//     address_street: 'Astora St',
+//     address_zipcode: '36571'})
+//   .returning(['id', 'name'])
+//   .then(result => console.log(result));
+
+//Create three restaurants and return id and name
+// knex('restaurants')
+//   .insert(
+//     [{name: 'Sunlit Cafe',
+//     borough: 'Brooklyn',
+//     cuisine: 'breakfast',
+//     address_building_number: '444',
+//     address_street: 'Astora St',
+//     address_zipcode: '36571'},
+
+//     {name: 'Darkwraith Tavern',
+//     borough: 'Brooklyn',
+//     cuisine: 'bar',
+//     address_building_number: '445',
+//     address_street: 'Astora St',
+//     address_zipcode: '36571'},
+
+//     {name: 'Bed and Breakfast of Chaos',
+//     borough: 'Brooklyn',
+//     cuisine: 'breakfast',
+//     address_building_number: '445',
+//     address_street: 'Astora St',
+//     address_zipcode: '36571'}
+//   ])
+//   .returning(['id', 'name'])
+//   .then(result => console.log(result));
+
+// Update record w/ nyc_restaurant_id 30191841
+// knex('restaurants')
+//   .where('nyc_restaurant_id', '30191841')
+//   .update('name', 'DJ Reynolds Pub and Restaurant')
+//   .then(result => console.log(result));
+
+// Delete record 10
+// knex('grades')
+//   .where('id', '10')
+//   .del()
+//   .then(result => console.log(result));
+
+// Delete restaurant id 22
+// knex('restaurants')
+// .where('id', '22')
+//   .del()
+//   .then(result => console.log(result));
